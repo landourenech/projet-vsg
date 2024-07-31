5766ae3e2259cb2c66b4838e921c86bc6bd6b05b
ASSEKO <assekoazareel222@gmail.com>
2024-07-31 06:48:07 +0100

    troisieme version

--- src/app/connexion/page.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./connexion.module.css"
-
-export default function connexion(){
-    return (
-        <div className={styles.titre}>
-           <h1>guyshgfbrhjfnvewfnhrfunun</h1> 
-        </div>
-    );
-}
